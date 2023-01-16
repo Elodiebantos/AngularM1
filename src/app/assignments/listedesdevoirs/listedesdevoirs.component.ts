19f96ce017234896e85aa4cbc0deeae7562145f4
Elodiebantos <56624671+Elodiebantos@users.noreply.github.com>
2023-01-16 19:29:36 +0100

    mat paginator ok + rendu clignote

--- src/app/assignments/listedesdevoirs/listedesdevoirs.component.ts
+++ src/app/assignments/listedesdevoirs/listedesdevoirs.component.ts
@@ -4,6 +4,7 @@ import { TasksService } from '../../services/tasks.service';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table'
 
 import { BDTasks } from '../data';
 @Component({
@@ -13,24 +14,31 @@ import { BDTasks } from '../data';
 })
 export class ListedesdevoirsComponent implements OnInit {
   tasks: Task[] = [];
-  showAddTask!:boolean;
+  showAddTask!: boolean;
   totalTasks: number = this.tasks.length;
   subscription: Subscription;
-  displayedColumns: string[] = ['auteur','matiere','nom', 'dateDeRendu','note', 'rendu', 'reminder','remarque', 'photoProf', 'supprimer' ];
+  dataSource = new MatTableDataSource<Task>(this.tasks);
 
-  @ViewChild(MatPaginator, {static: false}) paginator!: MatPaginator;
-  
-  constructor(public taskService: TasksService,public uiService:UiService){
+  displayedColumns: string[] = ['auteur', 'matiere', 'nom', 'dateDeRendu', 'note', 'rendu', 'reminder', 'remarque', 'photoProf', 'supprimer'];
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+
+  constructor(public taskService: TasksService, public uiService: UiService) {
     this.subscription = this.uiService
-    .onToggle()
-    .subscribe((value) => (this.showAddTask = value));
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
 
 
   ngOnInit(): void {
     this.taskService.getTasks()
-    .subscribe((tasks) => this.tasks = tasks
+      .subscribe((tasks: Task[]) => {
+        this.tasks = tasks
+        this.dataSource = new MatTableDataSource<Task>(this.tasks);
+        this.dataSource.paginator = this.paginator;
+      }
     );
+
   }
 
   deleteTask(task: Task) {
@@ -43,8 +51,8 @@ export class ListedesdevoirsComponent implements OnInit {
   toggleRendu(task: Task) {
     task.rendu = !task.rendu;
     this.taskService
-    .updateTaskRendu(task)
-    .subscribe();
+      .updateTaskRendu(task)
+      .subscribe();
   }
 
   toggleReminder(task: Task) {
@@ -52,46 +60,44 @@ export class ListedesdevoirsComponent implements OnInit {
     this.taskService.updateTaskReminder(task).subscribe();
   }
 
-  addNewTask(task:Task){
+  addNewTask(task: Task) {
     this.taskService
       .addTask(task)
       .subscribe(task => this.tasks.push(task));
   }
 
-  addTask(task:Task, boole:Boolean){ 
+  addTask(task: Task, boole: Boolean) {
     this.taskService
-      .addTask(task).subscribe((task) => {this.tasks.push(task); if(boole) window.location.reload()});
+      .addTask(task).subscribe((task) => { this.tasks.push(task); if (boole) window.location.reload() });
   }
 
-  toggleAddTask(){
+  toggleAddTask() {
     this.uiService.toggleAddTask();
   }
 
-  toggle
 
-
-  peuplerBD(){
-    const matieres = ["Anglais", "Java", "Base de données", "Développement Node", "Spring Boot", "Grails", "Italien","Portugais","Espagnol","Dév Back-end","ReactJS","Administration de serveur", "Allemand", "Développement C#", ".NET", "C++", "HTML5 / CSS3", "Javascript", "Front-end", "Développement Serveur Node", "Virtual Box", "VMware"];
+  peuplerBD() {
+    const matieres = ["Anglais", "Java", "Base de données", "Développement Node", "Spring Boot", "Grails", "Italien", "Portugais", "Espagnol", "Dév Back-end", "ReactJS", "Administration de serveur", "Allemand", "Développement C#", ".NET", "C++", "HTML5 / CSS3", "Javascript", "Front-end", "Développement Serveur Node", "Virtual Box", "VMware"];
     const noms = ["Examen", "TD", "TP", "Contrôle", "Leçon", "Interro Orale", "Soutenance", "Présentation", "Rapport", "Powerpoint"]
-    for(let i=1; i<=1000 ;i++){
-      let booleenRendu=Math.random() < 0.5;
-      let noteRendu = booleenRendu? Math.floor(Math.random() * 20) : undefined
-      let modulo= i>70?i%70:i;
-      const newTask:Task = {
+    for (let i = 1; i <= 1000; i++) {
+      let booleenRendu = Math.random() < 0.5;
+      let noteRendu = booleenRendu ? Math.floor(Math.random() * 20) : undefined
+      let modulo = i > 70 ? i % 70 : i;
+      const newTask: Task = {
         auteur: "Elodie",
-        matiere : matieres[Math.floor(Math.random() * matieres.length)],
+        matiere: matieres[Math.floor(Math.random() * matieres.length)],
         nom: noms[Math.floor(Math.random() * noms.length)],
-        id : i,
-        dateDeRendu : new Date(new Date(2023, 0, 1).getTime() + Math.random() * (new Date(2024, 0, 1).getTime() - new Date(2023, 0, 1).getTime())),
-        rendu : booleenRendu,
-        reminder : Math.random() < 0.5,
+        id: i,
+        dateDeRendu: new Date(new Date(2023, 0, 1).getTime() + Math.random() * (new Date(2024, 0, 1).getTime() - new Date(2023, 0, 1).getTime())),
+        rendu: booleenRendu,
+        reminder: Math.random() < 0.5,
         note: noteRendu,
-        remarque: Math.random() < 0.5? "une remarque utile" : "",
+        remarque: Math.random() < 0.5 ? "une remarque utile" : "",
         //photoProf:"https://thispersondoesnotexist.com/image"
-        photoProf:"https://i.pravatar.cc/150?img="+modulo
+        photoProf: "https://i.pravatar.cc/150?img=" + modulo
       }
-        console.log(newTask)
-        this.addTask(newTask,i==1000)
+      console.log(newTask)
+      this.addTask(newTask, i == 1000)
     }
   }
 }
